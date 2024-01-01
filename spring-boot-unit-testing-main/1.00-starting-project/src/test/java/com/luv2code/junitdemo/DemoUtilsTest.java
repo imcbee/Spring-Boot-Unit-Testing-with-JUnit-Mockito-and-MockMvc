e882f214925d3c3e1f1c21009fb54d547b66a6ff
Ian McBee <mcbee_ian@bah.com>
2024-01-01 09:40:33 -0500

    before and after each & all

--- spring-boot-unit-testing-main/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ spring-boot-unit-testing-main/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,14 +1,39 @@
 package com.luv2code.junitdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+  @BeforeEach
+  void setUpBeforeEach() {
+    demoUtils = new DemoUtils();
+    System.out.println("@BeforeEach executes before the execution of each test method");
+  }
+
+  @BeforeAll
+  static void setUpBeforeEachClass() {
+    System.out.println("@BeforeAll executes only once before all test methods execution in the class");
+  }
+
+  @AfterAll
+  static void tearDownAfterAll() {
+    System.out.println("@AfterAll executes only once after all test methods execution in the class");
+  }
+
+
+  @AfterEach
+  void tearDownAfterEach() {
+    System.out.println("Running @AfterEach");
+    System.out.println();
+  }
+
   @Test
   void testEqualsAndNotEquals() {
-    DemoUtils demoUtils = new DemoUtils();
+    System.out.println("Running test: testEqualsAndNotEquals");
 
     assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
     assertNotEquals(6, demoUtils.add(1, 9), "1+9 must be 6");
@@ -16,6 +41,7 @@ class DemoUtilsTest {
 
   @Test
   void testNullAndNotNull() {
+    System.out.println("Running test: testNullAndNotNull");
     DemoUtils demoUtils = new DemoUtils();
 
     String str1 = null;
