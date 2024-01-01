f14bf29edfc50fb4ee0b1c66471d3465b9239d91
Ian McBee <mcbee_ian@bah.com>
2024-01-01 18:29:16 -0500

    conditional testing

---
+++ spring-boot-unit-testing-main/1.00-starting-project/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -0,0 +1,63 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+class ConditionalTest {
+
+  @Test
+  @Disabled("Don't run until JIRA #123 is resolved...")
+  void basicTest() {
+    // execute method and perform asserts
+  }
+
+  @Test
+  @EnabledOnOs(OS.WINDOWS)
+  void testForWindowsOnly() {
+    // execute method and perform asserts
+  }
+
+  @Test
+  @EnabledOnOs(OS.MAC)
+  void testForMacOnly() {
+    // execute method and perform asserts
+  }
+
+  @Test
+  @EnabledOnOs({OS.WINDOWS, OS.MAC})
+  void testForMacAndWindowsOnly() {
+    // execute method and perform asserts
+  }
+
+
+  @Test
+  @EnabledOnOs(OS.LINUX)
+  void testForLinuxOnly() {
+    // execute method and perform asserts
+  }
+
+  @Test
+  @EnabledOnJre(JRE.JAVA_17)
+  void testForJava17() {
+    // execute method and perform asserts
+  }
+
+  @Test
+  @EnabledOnJre(JRE.JAVA_13)
+  void testForJava13() {
+    // execute method and perform asserts
+  }
+
+  @Test
+  @EnabledForJreRange(min=JRE.JAVA_13, max=JRE.JAVA_18)
+  void testForJavaRange() {
+    // execute method and perform asserts
+  }
+
+  @Test
+  @EnabledForJreRange(min=JRE.JAVA_11)
+  void testForJavaRangeMin() {
+    // execute method and perform asserts
+  }
+}
