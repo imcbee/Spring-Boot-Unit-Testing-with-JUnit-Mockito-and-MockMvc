6582fb274a137b95452b450ba11c224d12ff7e3b
Ian McBee <mcbee_ian@bah.com>
2024-01-01 19:04:40 -0500

    Refactor FizzBuzz with TDD

--- spring-boot-unit-testing-main/1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ spring-boot-unit-testing-main/1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -2,18 +2,37 @@ package com.luv2code.tdd;
 
 public class FizzBuzz {
 
+
   public static String compute(int i) {
 
-    if((i % 3 == 0) && (i % 5 == 0)) {
-      return "FizzBuzz";
-    }
+    StringBuilder result = new StringBuilder();
+
     if(i % 3 == 0) {
-      return "Fizz";
-    } else if (i % 5 ==9) {
-      return "Buzz";
-    }else {
-      return Integer.toString(i);
+      result.append("Fizz");
     }
 
+    if(i % 5 == 0) {
+      result.append("Buzz");
+    }
+
+    if(result.isEmpty()) {
+      result.append(i);
+    }
+
+    return result.toString();
   }
+//  public static String compute(int i) {
+//
+//    if((i % 3 == 0) && (i % 5 == 0)) {
+//      return "FizzBuzz";
+//    }
+//    if(i % 3 == 0) {
+//      return "Fizz";
+//    } else if (i % 5 ==9) {
+//      return "Buzz";
+//    }else {
+//      return Integer.toString(i);
+//    }
+//
+//  }
 }
